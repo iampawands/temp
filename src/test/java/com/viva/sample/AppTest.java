32e4d4b7ead9cc80ba6e9803b3e1787c476d4c93
iampawands <54079343+iampawands@users.noreply.github.com>
2019-11-05 01:37:24 +0530

    done dona

---
+++ src/test/java/com/viva/sample/AppTest.java
@@ -0,0 +1,12 @@
+package com.viva.sample;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest{
+	@Test
+	public void doThing() {
+		assertTrue(2+3==5);
+	}
+}
